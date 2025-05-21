159032de808735e2245400404cd9d7d01f1af5b2
ritikops <ritiks.career@gmail.com>
2025-05-21 13:42:49 +0530

    may21

--- Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/main.tf
+++ Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/main.tf
@@ -1,19 +1,43 @@
+# # module "vpc" {
+# #   source = "./vpc"
+# #   primary_region   = var.primary_region
+# #   secondary_region = var.secondary_region
+# # }
+
 # module "vpc" {
-#   source           = "./vpc"
+#   source = "./vpc"
+
+#   providers = {
+#     aws.primary   = aws.primary
+#     aws.secondary = aws.secondary
+#   }
+
 #   primary_region   = var.primary_region
 #   secondary_region = var.secondary_region
 # }
 
+# # module "aurora" {
+# #   source            = "./aurora"
+# #   db_password       = var.db_password
+# #   db_name           = var.db_name
+# #   db_username       = var.db_username
+# #   global_cluster_id = var.global_cluster_id
+# # }
+
 # module "aurora" {
-#   source             = "./aurora"
-#   db_engine          = var.db_engine
-#   engine_version     = var.engine_version
-#   instance_class     = var.instance_class
-#   db_name            = var.db_name
-#   db_username        = var.db_username
-#   db_password        = var.db_password
-#   global_cluster_id  = var.global_cluster_id
+#   source            = "./aurora"
+
+#   providers = {
+#     aws.primary   = aws.primary
+#     aws.secondary = aws.secondary
+#   }
+
+#   db_password       = var.db_password
+#   db_name           = var.db_name
+#   db_username       = var.db_username
+#   global_cluster_id = var.global_cluster_id
 # }
+
 
 # module "lambda" {
 #   source            = "./lambda"
@@ -24,8 +48,24 @@
 #   db_hostname       = var.db_hostname
 # }
 
+# module "lambda" {
+#   source = "./lambda"
+
+#   providers = {
+#     aws.primary   = aws.primary
+#     aws.secondary = aws.secondary
+#   }
+
+#   primary_region    = var.primary_region
+#   secondary_region  = var.secondary_region
+#   global_cluster_id = var.global_cluster_id
+#   hosted_zone_id    = var.hosted_zone_id
+#   db_hostname       = var.db_hostname
+# }
+
+
 # module "route53" {
-#   source         = "./route53"
+#   source        = "./route53"
 #   hosted_zone_id = var.hosted_zone_id
 #   db_hostname    = var.db_hostname
 # }
@@ -33,4 +73,77 @@
 # module "cloudwatch" {
 #   source            = "./cloudwatch"
 #   global_cluster_id = var.global_cluster_id
+# # }
+# provider "aws" {
+#   alias  = "primary"
+#   region = var.primary_region
 # }
+
+# provider "aws" {
+#   alias  = "secondary"
+#   region = var.secondary_region
+# }
+
+
+module "vpc" {
+  source = "./vpc"
+  providers = {
+    aws.primary   = aws.primary
+    aws.secondary = aws.secondary
+  }
+  primary_region   = var.primary_region
+  secondary_region = var.secondary_region
+}
+
+module "aurora" {
+  source = "./aurora"
+  # providers = {
+  #   aws.primary   = aws.primary
+  #   aws.secondary = aws.secondary
+  # }
+  primary_region      = var.primary_region
+  secondary_region    = var.secondary_region
+  db_password         = var.db_password
+  global_cluster_id   = var.global_cluster_id
+  subnet_ids           = module.vpc.primary_subnet_ids
+  primary_subnet_ids   = module.vpc.primary_subnet_ids
+  secondary_subnet_ids = module.vpc.secondary_subnet_ids
+}
+# Repeat for lambda, cloudwatch, route53 modules if they use aliased providers
+
+module "lambda" {
+  source = "./lambda"
+  providers = {
+    aws.primary   = aws.primary
+    aws.secondary = aws.secondary
+  }
+  primary_region    = var.primary_region
+  secondary_region  = var.secondary_region
+  global_cluster_id = var.global_cluster_id
+  hosted_zone_id    = var.hosted_zone_id
+  db_hostname       = var.db_hostname
+  function_name     = var.function_name
+}
+
+module "cloudwatch" {
+  source = "./cloudwatch"
+  providers = {
+    aws.primary   = aws.primary
+    aws.secondary = aws.secondary
+  }
+  global_cluster_id    = var.global_cluster_id
+  lambda_function_arn  = module.lambda.failover_lambda_arn
+  lambda_function_name = module.lambda.failover_lambda_name
+}
+
+module "route53" {
+  source = "./route53"
+  providers = {
+    aws.primary   = aws.primary
+    aws.secondary = aws.secondary
+  }
+  hosted_zone_id      = var.hosted_zone_id
+  db_hostname         = var.db_hostname
+  primary_endpoint    = module.aurora.primary_endpoint
+  secondary_endpoint  = module.aurora.secondary_endpoint
+}
