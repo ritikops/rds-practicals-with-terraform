fe0af94f7811a527da24020bd7450f8a7284a515
ritikops <ritiks.career@gmail.com>
2025-05-22 10:17:05 +0530

    changes added

--- Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/aurora/secondary_clusrter.tf
+++ Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/aurora/secondary_clusrter.tf
@@ -1,16 +1,21 @@
-# resource "aws_rds_cluster" "secondary" {
-#   provider                  = aws.secondary
-#   cluster_identifier        = "${var.global_cluster_id}-secondary"
-#   engine                    = var.db_engine
-#   engine_version            = var.engine_version
-#   db_subnet_group_name      = aws_db_subnet_group.aurora.name
-#   global_cluster_identifier = aws_rds_global_cluster.global.id
-#   skip_final_snapshot       = true
-# }
+resource "aws_rds_cluster" "secondary" {
+  provider = aws.secondary
+  cluster_identifier        = "aurora-cluster-secondary"
+  engine                    = var.db_engine
+  engine_version            = var.engine_version
+  global_cluster_identifier = var.global_cluster_id
+  db_subnet_group_name      = aws_db_subnet_group.secondary.name
+  availability_zones        = ["us-west-2a", "us-west-2b"]
+  skip_final_snapshot       = true
+  master_username           = var.db_username
+  master_password           = var.db_password
 
-# resource "aws_rds_cluster_instance" "secondary_inst" {
-#   provider           = aws.secondary
-#   identifier         = "${var.global_cluster_id}-secondary-1"
-#   cluster_identifier = aws_rds_cluster.secondary.id
-#   instance_class     = var.instance_class
-# }
+}
+
+resource "aws_rds_cluster_instance" "secondary_inst" {
+  provider           = aws.secondary
+  identifier         = "${var.global_cluster_id}-secondary-1"
+  cluster_identifier = aws_rds_cluster.secondary.id
+  instance_class     = var.instance_class
+  engine             = var.db_engine
+}
--- Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/aurora/subnet_group.tf
+++ Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/aurora/subnet_group.tf
@@ -1,7 +1,22 @@
-# resource "aws_db_subnet_group" "aurora" {
-#   name       = "aurora-subnet-group"
-#   subnet_ids = concat(
-#     var.primary_subnet_ids,
-#     var.secondary_subnet_ids
-#   )
-# }
+resource "aws_db_subnet_group" "primary" {
+  name       = "aurora-primary-subnet-group"
+  subnet_ids = var.primary_subnet_ids
+
+  tags = {
+    Name = "Aurora Primary DB Subnet Group"
+  }
+}
+
+resource "aws_db_subnet_group" "secondary" {
+  provider = aws.secondary
+  name       = "aurora-secondary-subnet-group"
+  subnet_ids = var.secondary_subnet_ids
+  # subnet_ids = [
+  #   aws_subnet.secondary_a.id,
+  #   aws_subnet.secondary_b.id
+  # ]
+
+  tags = {
+    Name = "Aurora Secondary DB Subnet Group"
+  }
+}
