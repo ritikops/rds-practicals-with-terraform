24498cc58e1a4ed68165434c023be4b5e7c89b0c
ritikops <ritiks.career@gmail.com>
2025-05-23 14:50:07 +0530

    Remove global RDS module files and related configurations

--- Task-4-2/global_rds/modules/global-database/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cluster_identifier" {
-  description = "The identifier for the global cluster"
-  type        = string
-}
-variable "engine_version" {
-  description = "The engine version to use for the RDS global cluster"
-  type        = string
-}
