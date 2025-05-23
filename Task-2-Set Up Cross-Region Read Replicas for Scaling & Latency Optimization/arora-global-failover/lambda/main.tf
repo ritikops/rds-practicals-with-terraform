abf8df61d943fedcb7aad0414bced914fb13ae51
ritikops <ritiks.career@gmail.com>
2025-05-23 11:05:00 +0530

    `Removed and re-added aws_iam_role "lambda_failover" resource in main.tf`

--- Task-2-Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/lambda/main.tf
+++ Task-2-Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/lambda/main.tf
@@ -73,16 +73,16 @@ resource "aws_lambda_function" "failover" {
     }
   }
 }
-resource "aws_iam_role" "lambda_failover" {
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = { Service = "lambda.amazonaws.com" }
-    }]
-  })
-}
+# resource "aws_iam_role" "lambda_failover" {
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Action = "sts:AssumeRole",
+#       Effect = "Allow",
+#       Principal = { Service = "lambda.amazonaws.com" }
+#     }]
+#   })
+# }
 # resource "aws_iam_role_policy" "lambda_policy" {
 #   name = "${var.function_name}-lambda-policy"
 #   role = aws_iam_role.lambda_exec.id
