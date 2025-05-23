1fb148085f11b0366dd09bdd898225c68a1ab91b
ritikops <ritiks.career@gmail.com>
2025-05-23 09:08:13 +0530

    changes in aurora

--- Task-2-Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/aurora/outputs.tf
+++ Task-2-Set Up Cross-Region Read Replicas for Scaling & Latency Optimization/arora-global-failover/aurora/outputs.tf
@@ -8,3 +8,14 @@ output "secondary_endpoint" {
 output "secondary_cluster_id" {
   value = aws_rds_cluster.secondary.id
 }
+output "primary_cluster_identifier" {
+  value = aws_rds_cluster.primary.id
+}
+
+output "replica_cluster_identifier" {
+  value = aws_rds_cluster.secondary.id
+}
+
+output "global_cluster_identifier" {
+  value = aws_rds_global_cluster.global.id
+}
