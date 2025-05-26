80c3f807d78af27e8116de0b77cc9fb6ae05e9fa
ritikops <ritiks.career@gmail.com>
2025-05-26 19:25:57 +0530

    made changes to variables.tf

--- Task-5-Cost Optimization & Replica Scaling Using Lambda Scheduler/variables.tf
+++ Task-5-Cost Optimization & Replica Scaling Using Lambda Scheduler/variables.tf
@@ -1,25 +1,25 @@
-variable "db_instance_identifier" {
-  description = "The identifier of the RDS DB instance."
-  type        = string
-}
+# variable "db_instance_identifier" {
+#   description = "The identifier of the RDS DB instance."
+#   type        = string
+# }
 
-variable "read_replica_instance_class" {
-  description = "The instance class for the read replicas."
-  type        = string
-  default     = "db.t3.medium"
-}
+# variable "read_replica_instance_class" {
+#   description = "The instance class for the read replicas."
+#   type        = string
+#   default     = "db.t3.medium"
+# }
 
-variable "scale_up_schedule_expression" {
-  description = "Cron expression for scaling up read replicas."
-  type        = string
-  default     = "cron(0 9 * * ? *)" # Every day at 9 AM UTC
-}
+# variable "scale_up_schedule_expression" {
+#   description = "Cron expression for scaling up read replicas."
+#   type        = string
+#   default     = "cron(0 9 * * ? *)" # Every day at 9 AM UTC
+# }
 
-variable "scale_down_schedule_expression" {
-  description = "Cron expression for scaling down read replicas."
-  type        = string
-  default     = "cron(0 19 * * ? *)" # Every day at 7 PM UTC
-}
+# variable "scale_down_schedule_expression" {
+#   description = "Cron expression for scaling down read replicas."
+#   type        = string
+#   default     = "cron(0 19 * * ? *)" # Every day at 7 PM UTC
+# }
 
 variable "db_instance_identifier" {
   description = "The identifier of the RDS DB instance."
@@ -68,8 +68,9 @@ variable "desired_read_replica_count" {
     condition     = var.desired_read_replica_count >= 0
     error_message = "The desired number of read replicas must be a non-negative integer."
   }
-}variable "desired_read_replica_count" {
-  description = "Desired number of read replicas during business hours."
-  type        = number
-  default     = 2
 }
+# variable "desired_read_replica_count" {
+#   description = "Desired number of read replicas during business hours."
+#   type        = number
+#   default     = 2
+# }
