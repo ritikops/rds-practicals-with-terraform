7cab4f98da858a7660dd3d64c80b5628540b806c
ritikops <ritiks.career@gmail.com>
2025-05-26 19:18:27 +0530

    edit main.tf to include lambda monitoring module

--- Task-6-Implement-Event-Driven-Backups-Monitoring-with-Lambda/main.tf
+++ Task-6-Implement-Event-Driven-Backups-Monitoring-with-Lambda/main.tf
@@ -1,30 +1,53 @@
 
-## Root module - main.tf
+# ## Root module - main.tf
 
-module "rds_global_cluster" {
-  source = "./modules/rds"
-}
+# module "rds_global_cluster" {
+#   source = "./modules/rds"
+# }
 
-module "lambda_monitoring" {
-  source                = "./modules/lambda"
-  snapshot_s3_bucket    = module.rds_global_cluster.snapshot_s3_bucket
-  rds_global_cluster_id = module.rds_global_cluster.global_cluster_id
-  sns_topic_arn         = module.notifications.sns_topic_arn
+# module "lambda_monitoring" {
+#   source                = "./modules/lambda"
+#   snapshot_s3_bucket    = module.rds_global_cluster.snapshot_s3_bucket
+#   rds_global_cluster_id = module.rds_global_cluster.global_cluster_id
+#   sns_topic_arn         = module.notifications.sns_topic_arn
+# }
+
+# module "notifications" {
+#   source = "./modules/notifications"
+# }
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 4.0"
+#     }
+#   }
+# }
+
+module "rds" {
+  source            = "./modules/rds"
+  master_username   = var.master_username
+  master_password   = var.master_password
+  subnet_ids        = var.subnet_ids
+  security_group_id = var.security_group_id
+  azs               = var.azs
 }
 
 module "notifications" {
   source = "./modules/notifications"
+  email  = var.alert_email
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
-  }
+module "lambda" {
+  source                = "./modules/lambda"
+  snapshot_s3_bucket    = module.rds.snapshot_s3_bucket
+  rds_global_cluster_id = module.rds.global_cluster_id
+  sns_topic_arn         = module.notifications.sns_topic_arn
+  kms_key_id            = var.kms_key_id
+  export_role_arn       = var.export_role_arn
 }
