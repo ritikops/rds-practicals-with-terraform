cf41493e228d66872d0904f2b08980f91b8f6b68
ritikops <ritiks.career@gmail.com>
2025-05-27 13:56:16 +0530

    changes to the rds module in Task-6

--- Task-6-Implement-Event-Driven-Backups-Monitoring-with-Lambda/modules/rds/outputs.tf
+++ Task-6-Implement-Event-Driven-Backups-Monitoring-with-Lambda/modules/rds/outputs.tf
@@ -1,10 +1,3 @@
-output "global_cluster_id" {
-  value = aws_rds_global_cluster.this.id
-}
-
-output "snapshot_s3_bucket" {
-  value = aws_s3_bucket.snapshot_backup.bucket
-}
 output "endpoint" {
   value = aws_rds_cluster.primary.endpoint
 }
--- Task-6-Implement-Event-Driven-Backups-Monitoring-with-Lambda/modules/rds/variables.tf
+++ Task-6-Implement-Event-Driven-Backups-Monitoring-with-Lambda/modules/rds/variables.tf
@@ -1,22 +1,3 @@
-variable "master_username" {
-  default = "admin"
-}
-
-variable "master_password" {
-  default = "Password1234!"
-}
-
-variable "subnet_ids" {
-  type = list(string)
-}
-
-variable "security_group_id" {
-  type = string
-}
-
-variable "azs" {
-  type = list(string)
-}
 variable "cluster_name" {}
 variable "primary_region" {}
 variable "secondary_region" {}
