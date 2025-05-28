58c446f577ff5566eac6e59b866424ff3c323296
ritikops <ritiks.career@gmail.com>
2025-05-28 15:33:06 +0530

    made changes in iam module

--- Task-7-Cost Optimization & Replica Scaling Using Lambda Scheduler/modules/iam/main.tf
+++ Task-7-Cost Optimization & Replica Scaling Using Lambda Scheduler/modules/iam/main.tf
@@ -1,28 +1,31 @@
 resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda-rds-scaling-role"
+  name               = "lambda-rds-scaling-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Action = "sts:AssumeRole"
-      Principal = { Service = "lambda.amazonaws.com" }
       Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
     }]
   })
+  description = "IAM role for Lambda to manage RDS scaling"
+  path        = "/"
 }
 
-resource "aws_iam_policy_attachment" "lambda_basic_execution" {
-  name       = "attach-lambda-basic-exec"
-  roles      = [aws_iam_role.lambda_exec_role.name]
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_policy" "rds_scaling_policy" {
-  name   = "rds-scaling-policy"
-  policy = file("${path.module}/policies/rds-scaling-policy.json")
+  name        = "rds-scaling-policy"
+  description = "Policy to allow RDS scaling operations from Lambda"
+  policy      = file("${path.module}/policies/rds-scaling-policy.json")
 }
 
-resource "aws_iam_policy_attachment" "rds_scaling_attach" {
-  name       = "attach-rds-scaling"
-  roles      = [aws_iam_role.lambda_exec_role.name]
+resource "aws_iam_role_policy_attachment" "rds_scaling_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.rds_scaling_policy.arn
 }
