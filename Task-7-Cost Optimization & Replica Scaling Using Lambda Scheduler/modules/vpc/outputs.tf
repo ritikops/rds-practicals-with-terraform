65f899e505e70b70da2a5e40de5fbd939ace1350
ritikops <ritiks.career@gmail.com>
2025-05-28 15:46:34 +0530

    made changes in vpc output.tf

--- Task-7-Cost Optimization & Replica Scaling Using Lambda Scheduler/modules/vpc/outputs.tf
+++ Task-7-Cost Optimization & Replica Scaling Using Lambda Scheduler/modules/vpc/outputs.tf
@@ -9,3 +9,6 @@ output "private_subnet_ids" {
 output "subnet_group_name" {
   value = aws_db_subnet_group.rds_subnet_group.name
 }
+output "db_sg_id" {
+  value = aws_db_subnet_group.rds_subnet_group.id
+}
